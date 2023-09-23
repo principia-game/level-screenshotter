3de142e683fabe0d3b37fe1ca3a943e07df91148
ROllerozxa <rollerozxa@voxelmanip.se>
2023-09-23 21:41:45 +0200

    Update screenshotter scripts

--- screenshotter-bot.py
+++ screenshotter-bot.py
@@ -7,7 +7,7 @@ from config import *
 
 def main():
 	http = urllib3.PoolManager()
-	data = json.loads(http.request('GET','%s/internal/levels_with_no_thumbs.php' % DOMAIN).data)
+	data = json.loads(http.request('GET','%s/levels_with_no_thumbs' % DOMAIN).data)
 
 	os.system("principia &")
 	print("läumching principaa")
@@ -21,7 +21,8 @@ def main():
 		print("doing "+str(level))
 		screenshotter.main(level)
 
-	os.system("rsync -azP thumbs/* %s:%s/levels/thumbs/" % (SERVER_SSH, SERVER_DIR))
+	os.system("rsync -azP thumbs/* %s:%s/data/thumbs/" % (SERVER_SSH, SERVER_DIR))
+	os.system("rsync -azP thumbs_low/* %s:%s/data/thumbs_low/" % (SERVER_SSH, SERVER_DIR))
 
 if __name__ == "__main__":
 	main()
--- screenshotter.py
+++ screenshotter.py
@@ -6,8 +6,10 @@ from config import *
 from pathlib import Path
 
 def main(input_file, namespace = 'db'):
-	Path("thumbs/low/").mkdir(parents=True, exist_ok=True)
-	
+	Path("thumbs/").mkdir(exist_ok=True)
+	Path("thumbs_low/").mkdir(exist_ok=True)
+	Path("screenshots/").mkdir(exist_ok=True)
+
 	os.system("principia principia://principia-web.se/play/lvl/%s/%s &" % (namespace, input_file))
 	time.sleep(2)
 	image = ImageGrab.grab(bbox=(
@@ -16,9 +18,11 @@ def main(input_file, namespace = 'db'):
 		(MONITOR_WIDTH - PRINCIPIA_WIDTH) / 2 + PRINCIPIA_WIDTH,
 		((MONITOR_HEIGHT - PRINCIPIA_HEIGHT) / 2 + PRINCIPIA_HEIGHT)+29
 	))
-	image.save("thumbs/%s.%s" % (input_file, IMAGEFORMAT))
+	image.save("screenshots/%s.png" % (input_file))
 
-	os.system("convert thumbs/%s.%s -resize 240 -unsharp 0x0.55+0.55+0.008 -quality 90 thumbs/low/%s.%s" % (input_file, IMAGEFORMAT, input_file, IMAGEFORMAT))
+	os.system("convert screenshots/%s.png -quality 85 thumbs/%s.%s" % (input_file, input_file, IMAGEFORMAT))
+
+	os.system("convert thumbs/%s.%s -resize 240 -unsharp 0x0.55+0.55+0.008 -quality 92 thumbs_low/%s.%s" % (input_file, IMAGEFORMAT, input_file, IMAGEFORMAT))
 
 if __name__ == "__main__":
 	if len(sys.argv) < 2:
