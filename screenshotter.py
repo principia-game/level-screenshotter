47f3533455727642ddb90e91c7734477a1d64403
ROllerozxa <rollerozxa@voxelmanip.se>
2024-03-28 15:45:56 +0100

    Rewrite screenshotter scripts to use screenshot build

--- screenshotter.py
+++ screenshotter.py
@@ -1,34 +1,54 @@
-from PIL import ImageGrab
-import os
-import sys
-import time
 from config import *
 from pathlib import Path
+import json
+import os
+import requests
+import time
+import urllib3
 
-def main(input_file, namespace = 'db'):
+headers = {'User-Agent': 'ohmygoshcuddles'}
+
+def screenshot(id):
 	Path("thumbs/").mkdir(exist_ok=True)
 	Path("thumbs_low/").mkdir(exist_ok=True)
 	Path("screenshots/").mkdir(exist_ok=True)
 
-	os.system("principia principia://principia-web.se/play/lvl/%s/%s &" % (namespace, input_file))
-	time.sleep(2)
-	image = ImageGrab.grab(bbox=(
-		(MONITOR_WIDTH - PRINCIPIA_WIDTH) / 2,
-		((MONITOR_HEIGHT - PRINCIPIA_HEIGHT) / 2)+29,
-		(MONITOR_WIDTH - PRINCIPIA_WIDTH) / 2 + PRINCIPIA_WIDTH,
-		((MONITOR_HEIGHT - PRINCIPIA_HEIGHT) / 2 + PRINCIPIA_HEIGHT)+29
-	))
-	image.save("screenshots/%s.png" % (input_file))
+	data = requests.get(f"https://principia-web.se/internal/get_level?i={id}", headers=headers)
 
-	os.system("convert screenshots/%s.png -quality 85 thumbs/%s.%s" % (input_file, input_file, IMAGEFORMAT))
+	if data.status_code == 200:
+		with open(f".p/storage/lvl/db/{id}.plvl", 'wb') as file:
+			file.write(data.content)
+	else:
+		print("wah >.<")
+		return
 
-	os.system("convert thumbs/%s.%s -resize 240 -unsharp 0x0.55+0.55+0.008 -quality 92 thumbs_low/%s.%s" % (input_file, IMAGEFORMAT, input_file, IMAGEFORMAT))
+	os.system(f"./.p/principia principia://principia-web.se/play/lvl/db/{id}")
+
+	statefile = Path(".p/principia.state")
+	while True:
+		time.sleep(0.25)
+		if not statefile.is_file():
+			break
+
+	raw_screenshot = f".p/ss-0.png"
+	screenshot = f"screenshots/{id}.png"
+
+	os.system(f"mv {raw_screenshot} {screenshot}")
+
+	os.system(f"convert {screenshot} -quality 85 thumbs/{id}.jpg")
+
+	os.system(f"convert {screenshot} -resize 240 -unsharp 0x0.55+0.55+0.008 -quality 92 thumbs_low/{id}.jpg")
+
+def main():
+	http = urllib3.PoolManager()
+	data = json.loads(http.request('GET','https://principia-web.se/internal/levels_with_no_thumbs').data)
+
+	for level in data['levels_with_no_thumbs']:
+		print("doing "+str(level))
+		screenshot(level)
+
+	os.system(f"rsync -azP thumbs/* {RSYNC_REMOTE}/data/thumbs/")
+	os.system(f"rsync -azP thumbs_low/* {RSYNC_REMOTE}/data/thumbs_low/")
 
 if __name__ == "__main__":
-	if len(sys.argv) < 2:
-		print("gimmie a level id uwu")
-	else:
-		if len(sys.argv) < 3:
-			main(sys.argv[1], sys.argv[2])
-		else:
-			main(sys.argv[1])
+	main()
